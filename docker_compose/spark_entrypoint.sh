06d7ef709c2854f0520e25d3e3100a1a029be512
Marharyta Ihnatsyeva <marharytaihnatsyeva@new-host.home>
2025-01-03 14:49:55 +0100

    added docker compose

--- docker_compose/spark_entrypoint.sh
+++ docker_compose/spark_entrypoint.sh
@@ -3,9 +3,6 @@
 mkdir -p /app;
 cd /scripts;
 echo "installing boto3 and pyspark";
-#pip install poetry=='1.8.1';
-#export POETRY_VIRTUALENVS_CREATE=false;
-#poetry install --no-root --no-interaction --without unit_tests,quality_checks,docs;
 pip install boto3;
 pip install pyspark;
 
